102013a36f4ab93368c5595dede5bf996e5e929a
palyamRohith72 <palyamrohith72@gmail.com>
2025-01-30 16:59:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.sidebar:
     options = option_menu(
         "Select the operation to perform",
         ["Attributes", "Compare DataFrames", "Merge & Join", "Update DataFrames", "Add & Delete", "Access & Modify Data", "Plot Data"],
-        icons=["info-circle", "columns", "pen", "pencil-square", "trash", "tools", "data"],
+        icons=["info-circle", "columns", "pen", "pencil-square", "trash", "tools", "book"],
         menu_icon='gear',
         default_index=0
     )
