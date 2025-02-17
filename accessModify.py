ce0a5b5b1126da958ee4b3a88eb235c764362050
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-17 21:58:21 +0530

    Update accessModify.py

--- accessModify.py
+++ accessModify.py
@@ -13,7 +13,7 @@ class AccessModify:
         tab1, tab2 = st.tabs(['Perform Operations', 'View Your Results'])
         
         with tab1:
-            col1, col2 = st.columns([1, 2])
+            col1, col2 = st.columns([1, 2],border=True)
             
             options = col1.radio("Please select the operation that you want to perform", 
                                 ['Access Data', 'Access Data (Advanced)', 'Modify Values', 'Modify Values (Advanced)'])
