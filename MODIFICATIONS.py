bc630124e06723ef3df6159849468087526aeba4
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-18 09:42:38 +0530

    Update MODIFICATIONS.py

--- MODIFICATIONS.py
+++ MODIFICATIONS.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-class DataFrameOperations:
+class MODIFICATIONS:
     def __init__(self, data):
         self.data = data
     
