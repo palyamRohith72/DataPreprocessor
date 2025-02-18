e57c292cfb60ffb7002c21c271115e3554bd6f2b
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-18 21:29:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ from AddPlots import Plots
 from MODIFICATIONS import *
 from FILTERS import Filters
 from MATHS import Maths
+from GENAI import GenerativeAI
 # Initialize session state for storing dataframes
 if "allData" not in st.session_state:
     st.session_state["allData"] = {}
@@ -27,7 +28,7 @@ selected_data = st.selectbox("Please select the dataframe to perform operation",
 with st.sidebar:
     options = option_menu(
         "Select the operation to perform",
-        ["Attributes", "Compare DataFrames","Modifications","Filterations","Mathematical & Statistical", "Merge & Join", "Update DataFrames", "Add & Delete", "Access & Modify Data", "Plot Data"],
+        ["Attributes", "Compare DataFrames","Modifications","Filterations","Mathematical & Statistical", "GEN AI", "Update DataFrames", "Add & Delete", "Access & Modify Data", "Plot Data"],
         icons=["info-circle", "columns","columns","search","columns", "pen", "pencil-square", "trash", "tools", "book"],
         menu_icon='gear',
         default_index=0
@@ -45,10 +46,10 @@ elif options == "Compare DataFrames":
         df = st.session_state["allData"][selected_data]
         compare = COMPARE(df)
         compare.display()
-elif options == "Merge & Join":
+elif options == "GEN AI":
     if selected_data:
         df = st.session_state["allData"][selected_data]
-        compare = Maths(df)
+        compare = GenerativeAI(df)
         compare.display()
 elif options == "Mathematical & Statistical":
     if selected_data:
