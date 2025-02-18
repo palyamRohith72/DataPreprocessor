cd81d0ef1a0f49011da01c28000db516605478be
palyamRohith72 <palyamrohith72@gmail.com>
2025-02-18 09:41:43 +0530

    Create MODIFICATIONS.py

---
+++ MODIFICATIONS.py
@@ -0,0 +1,75 @@
+import streamlit as st
+
+class DataFrameOperations:
+    def __init__(self, data):
+        self.data = data
+    
+    def display(self):
+        st.subheader("DataFrame Operations", divider='blue')
+        tab1, tab2, tab3 = st.tabs(["Perform Operations", "View Results", "Clear Memory"])
+        
+        with tab1:
+            col1, col2 = st.columns([1, 2], border=True)
+            
+            with col1:
+                st.subheader("Select Operation")
+                operation = st.radio(
+                    "Choose an operation:",
+                    [
+                        "Apply", "Apply & Map", "Aggregate", "Group By", "Sort Values", "Sort Index", "Add Suffix", "Add Prefix", 
+                        "Rename", "Set Index", "Bin Numeric", "Clean Names", "Concatenate Columns", "Encode Categorical", 
+                        "Expand Columns", "Factorize Columns"
+                    ]
+                )
+                
+                method_name = operation.lower().replace(" & ", "_").replace(" ", "_")
+                if hasattr(self, method_name):
+                    getattr(self, method_name)(col1, col2)
+    
+    def apply(self, col1, col2):
+        pass
+    
+    def apply_map(self, col1, col2):
+        pass
+    
+    def aggregate(self, col1, col2):
+        pass
+    
+    def group_by(self, col1, col2):
+        pass
+    
+    def sort_values(self, col1, col2):
+        pass
+    
+    def sort_index(self, col1, col2):
+        pass
+    
+    def add_suffix(self, col1, col2):
+        pass
+    
+    def add_prefix(self, col1, col2):
+        pass
+    
+    def rename(self, col1, col2):
+        pass
+    
+    def set_index(self, col1, col2):
+        pass
+    
+    def bin_numeric(self, col1, col2):
+        pass
+    
+    def clean_names(self, col1, col2):
+        pass
+    
+    def concatenate_columns(self, col1, col2):
+        pass
+    
+    def encode_categorical(self, col1, col2):
+        pass
+    
+    def expand_columns(self, col1, col2):
+        pass
+    
+    def factorize_columns(self, col1, col2):
+        pass
